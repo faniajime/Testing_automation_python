80b500340a20bd302d56140ae1e2dc85a58c0db3
Fabiola Jiménez-González <faniajime@gmail.com>
2022-11-21 12:56:02 -0600

    Modified wishlist test

--- wishlistTest.py
+++ wishlistTest.py
@@ -4,19 +4,27 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+
 class TestRegisterAccount(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-
-    def login(self):
+        self.driver = webdriver.Chrome()
         
+    def login(self):
+        self.driver.get("https://magento.softwaretestingboard.com/customer/account/login/")
+        self.driver.find_element(By.ID, "email").send_keys("pancho@gmail.com")
+        self.driver.find_element(By.ID, "pass").send_keys("pkRHK4eEMgWE8TQ")
+        time.sleep(5)
+        self.driver.find_element(By.CSS_SELECTOR, ".primary:nth-child(1) > #send2 > span").click()
+  
 
-    def test_search_in_python_org(self):
+    def test_add_to_wishlist(self):
         driver = self.driver
-        driver.get("https://magento.softwaretestingboard.com/olivia-1-4-zip-light-jacket.html")
+        self.login()
+        time.sleep(5)
+        self.driver.get("https://magento.softwaretestingboard.com/olivia-1-4-zip-light-jacket.html")
         self.assertIn("Olivia 1/4 Zip Light Jacket", driver.page_source)
         time.sleep(5)
-        wishListBtn = driver.find_element(By.XPATH, "//*[@id=\"maincontent\"]/div[2]/div/div[1]/div[5]/div/a[1]/span")
+        wishListBtn = self.driver.find_element(By.XPATH, "//*[@id=\"maincontent\"]/div[2]/div/div[1]/div[5]/div/a[1]/span")
         wishListBtn.click()
         time.sleep(5)
         self.assertIn("Olivia 1/4 Zip Light Jacket has been added to your Wish List.", driver.page_source)
